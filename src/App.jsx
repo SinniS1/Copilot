c5fc3efc5be4c43bfd76d72acd5a422367cb51e2
SinniS1 <sinniscode@gmail.com>
2023-04-15 20:00:49 +0530

    App changes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.scss'
+import Navbar from './Components/Navbar/Navbar'
+const App = () => {
+  return (
+    <div className="App">
+      <React.Fragment>
+        <Navbar />
+      </React.Fragment>
+    </div>
+  )
+}
+
+export default App
