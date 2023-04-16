1b79d91cd56f67243c2d7745b39d239d767a597a
SinniS1 <sinniscode@gmail.com>
2023-04-16 23:25:02 +0530

    Section1 start

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './App.scss'
 import Navbar from './Components/Navbar/Navbar'
+import Section1 from './Components/Section1/Section1'
 const App = () => {
   return (
     <div className="App">
-      <React.Fragment>
-        <Navbar />
-      </React.Fragment>
+      <Navbar />
+      <Section1 />
     </div>
   )
 }
