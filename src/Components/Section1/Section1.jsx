2ce7d8179f37ab7a5d871e75e608ac6b78afd84c
SinniS1 <sinniscode@gmail.com>
2023-04-17 12:23:32 +0530

    section1 done

--- src/Components/Section1/Section1.jsx
+++ src/Components/Section1/Section1.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import './Section1.scss'
 const Section1 = () => {
-  return <div>Section1</div>
+  return (
+    <div className="Section1">
+      <div className="container">
+        <span className="heading">Your AI pair programmer</span>
+        <p className="para">
+          GitHub Copilot uses the Open AI Codex to suggest code and entire
+          functions in real time, right from editor.
+        </p>
+        <div className="buttons">
+          <button className="btn1">
+            Start my free trial
+            <span className="arrow">{'->'}</span>
+          </button>
+          <button className="btn2">Compare plans</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Section1
