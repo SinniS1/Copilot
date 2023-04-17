588235bb1544aef832bdf18ac897e29490d94aea
SinniS1 <sinniscode@gmail.com>
2023-04-17 19:44:45 +0530

    typing effect animation done

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.scss'
 import Navbar from './Components/Navbar/Navbar'
 import Section1 from './Components/Section1/Section1'
+import Copilot from './Components/copilotEffect/Copilot'
 const App = () => {
   return (
     <div className="App">
       <Navbar />
       <Section1 />
+      <Copilot />
     </div>
   )
 }
---
+++ src/Components/copilotEffect/Copilot.jsx
@@ -0,0 +1,144 @@
+import { useState } from 'react'
+import './Copilot.scss'
+
+const Copilot = () => {
+  const generateSpans = () => {
+    const spans = []
+    for (let i = 1; i <= 17; i++) {
+      spans.push(
+        <span className="num" key={i}>
+          {i}
+        </span>
+      )
+    }
+    return spans
+  }
+
+  const [showText1, setShowText1] = useState(false)
+  const [showText2, setShowText2] = useState(false)
+  const [showText3, setShowText3] = useState(false)
+  const [float, setFloat] = useState(false)
+
+  const handleAnimationEnd1 = () => {
+    setShowText1(true)
+  }
+  const handleAnimationEnd2 = () => {
+    setShowText2(true)
+  }
+  const handleAnimationEnd3 = () => {
+    setShowText3(true)
+  }
+  const handleAnimationEnd4 = () => {
+    setFloat(true)
+  }
+
+  return (
+    <div className="Section2">
+      <div className="CopilotEffect">
+        <div className="Nav">
+          <div className="pages">
+            <span className="active-page">Sentiments.ts</span>
+            <span>write_sql.go</span>
+            <span>parse_expenses.py</span>
+            <span>addresses.pb</span>
+          </div>
+        </div>
+        <div className="Typing">
+          <span className="numberBar">{generateSpans()}</span>
+          <div className="typingSection">
+            <span className="white ">#!/usr/bin/env ts-node</span>
+            <span>
+              <br />
+            </span>
+            <span className="">
+              <span className="orange">import </span>
+              <span className="white">{`{ fetch  }`}</span>
+              <span className="orange"> from </span>
+              <span className="blue"> "fetch-h2" </span>
+              <span className="white">{`;`}</span>
+            </span>
+            <span>
+              <br />
+            </span>
+            <span
+              className={`gray type1  ${showText1 ? 'visible' : ''}`}
+              onAnimationEnd={handleAnimationEnd1}
+            >
+              // Determine whether the sentiment of the text is positive
+            </span>
+            <span
+              className={`gray  type2 ${showText2 ? 'visible' : ''}`}
+              onAnimationEnd={handleAnimationEnd2}
+            >
+              // Use a web service
+            </span>
+            <span
+              className={`type3  ${showText3 ? 'visible' : ''}`}
+              onAnimationEnd={handleAnimationEnd3}
+            >
+              <span className="orange">async function </span>
+              <span className="purple">is Positive </span>
+              <span className="white">{`(text: string): Promise<boolean> {`}</span>
+            </span>
+            {/* <br /> */}
+            <div
+              className={`float-animation ${float ? 'visible' : ''}`}
+              onAnimationEnd={handleAnimationEnd4}
+            >
+              <span className="line">
+                <span className="orange">&nbsp;&nbsp;&nbsp;&nbsp;const</span>
+                <span className="white">&nbsp;response</span>
+                <span className="blue">&nbsp;=&nbsp;</span>
+                <span className="orange">&nbsp;await&nbsp;</span>
+                <span className="purple">&nbsp;await</span>
+                <span className="blue">{`(\`http://text-processing.com/sentiment/\`,{`}</span>
+              </span>
+              <span className="blue line">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;method:&nbsp;"POST",
+              </span>
+              <span className="blue line">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;body:&nbsp;`text=
+                <span className="white">{`\${text}`}</span>`,
+              </span>
+              <span className="blue line">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;headers:&nbsp;{' '}
+                <span className="white line"> {`\{`} </span>
+              </span>
+              <span className="white line">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"Content-Type":&nbsp;"
+                <span className="blue">application/x-www-form-urlencoded</span>
+                ",
+              </span>
+              <span className="white line">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {`\},`}
+              </span>
+              <span className="white line">
+                &nbsp;&nbsp;&nbsp;&nbsp; {`\});`}
+              </span>
+              <span className="line">
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="orange">const</span>
+                <span className="white">&nbsp;json</span>
+                <span className="white">&nbsp;=&nbsp;</span>
+                <span className="orange">await&nbsp;</span>
+                <span className="white">response.</span>
+                <span className="purple">json{`()`};&nbsp;</span>
+              </span>
+              <span className="line">
+                &nbsp;&nbsp;&nbsp;&nbsp; <span className="orange">const</span>
+                <span className="white">json.</span>
+                <span className="blue">label === </span>
+                <span className="purple">"pos"</span>
+                <span className="white">{`;`}</span>
+              </span>
+              <span className="white line">&nbsp;{`}`}</span>
+              <div className="Cp-Button">Copilot</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Copilot
