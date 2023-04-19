9f24f89f605979f5e556c0650523315adfea30d0
SinniS1 <sinniscode@gmail.com>
2023-04-19 22:30:52 +0530

    app  & copilot changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import './App.scss'
 import Navbar from './Components/Navbar/Navbar'
 import Section1 from './Components/Section1/Section1'
 import Copilot from './Components/copilotEffect/Copilot'
+import Section2 from './Components/Section2/Section2'
 const App = () => {
   return (
     <div className="App">
       <Navbar />
       <Section1 />
-      <Copilot />
+      <Section2 />
     </div>
   )
 }
