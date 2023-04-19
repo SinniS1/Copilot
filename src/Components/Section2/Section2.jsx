021b6bd1816b417ad5d3a747d3e8c61f4be997a1
SinniS1 <sinniscode@gmail.com>
2023-04-19 22:30:32 +0530

    added section2

---
+++ src/Components/Section2/Section2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Section2.scss'
+import Copilot from '../copilotEffect/Copilot'
+const Section2 = () => {
+  return (
+    <div>
+      {/* TODO: made the copilot animation with conditonal statements to run one after another */}
+      <Copilot />
+    </div>
+  )
+}
+
+export default Section2
