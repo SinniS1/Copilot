126589d7d7b23309c955fb9f3957f83657a3bb83
SinniS1 <sinniscode@gmail.com>
2023-04-29 00:47:03 +0530

    Copilot Typing complete Dyanamic done

---
+++ src/Components/Section2/Section2NonTyping.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Section2NonTyping = () => {
+  return (
+    <>
+      <span className="white ">#!/usr/bin/env ts-node</span>
+      <span>
+        <br />
+      </span>
+      <span className="">
+        <span className="orange">import </span>
+        <span className="white">{`{ fetch  }`}</span>
+        <span className="orange"> from </span>
+        <span className="blue"> "fetch-h2" </span>
+        <span className="white">{`;`}</span>
+      </span>
+      <span>
+        <br />
+      </span>
+    </>
+  )
+}
+
+export default Section2NonTyping
