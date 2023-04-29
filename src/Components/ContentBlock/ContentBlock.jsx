84662be6423e104eb294a985ade589458fd57a27
SinniS1 <sinniscode@gmail.com>
2023-04-29 20:22:01 +0530

    contentBlock updated

--- src/Components/ContentBlock/ContentBlock.jsx
+++ src/Components/ContentBlock/ContentBlock.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './ContentBlock.scss'
 const ContentBlock = ({
   heading,
@@ -7,24 +7,65 @@ const ContentBlock = ({
   ParaFontSize,
   SlidingAnimation,
 }) => {
+  const SlidingRef = useRef(null)
+  const [runAnimation, setRunAnimation] = useState(false)
+  useEffect(() => {
+    const observer1 = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setRunAnimation(true)
+        } else {
+          setRunAnimation(false)
+        }
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.8,
+      }
+    )
+    if (SlidingAnimation) {
+      observer1.observe(SlidingRef.current)
+    }
+    return () => {
+      observer1.unobserve(SlidingRef.current)
+    }
+  }, [])
+
+  const [Sliding1End, setSliding1End] = useState(false)
+  const [Slding2End, setSlding2End] = useState(false)
+  const handleAnimationEnd1 = () => {
+    setSliding1End(true)
+  }
+  const handleAnimationEnd2 = () => {
+    setSlding2End(true)
+  }
   return (
     <>
       <div
         className="ContentBlock"
         style={{ alignItems: SlidingAnimation ? '' : 'center' }}
+        ref={SlidingRef}
       >
         <span
-          className={`heading ${SlidingAnimation ? 'Sliding1' : ''}`}
-          style={{ fontSize: headingFontSize }}
+          className={`heading ${runAnimation ? 'Sliding1' : ''}
+          ${Sliding1End ? 'visible' : ''}`}
+          style={{
+            fontSize: headingFontSize,
+          }}
+          onAnimationEnd={handleAnimationEnd1}
         >
           {heading}
         </span>
         <p
-          className={`para ${SlidingAnimation ? 'Sliding2' : ''}`}
+          className={`para ${runAnimation ? 'Sliding2' : ''}
+          ${Slding2End ? 'visible' : ''}
+          `}
           style={{
             fontSize: ParaFontSize,
-            textAlign: SlidingAnimation ? 'justify' : 'center',
+            textAlign: SlidingAnimation ? 'justify' : 'left',
           }}
+          onAnimationEnd={handleAnimationEnd2}
         >
           {paragraph}
         </p>
