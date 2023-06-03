3dd9f2541dcdcfcec3f75d30608e31b7f944ad1e
SinniS1 <sinniscode@gmail.com>
2023-06-03 20:37:00 +0530

    added locomotive smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.scss'
 import Navbar from './Components/Navbar/Navbar'
 import Section1 from './Components/Section1/Section1'
-import Copilot from './Components/copilotEffect/Copilot'
 import Section2 from './Components/Section2/Section2'
 import Section3 from './Components/Section3/Section3'
 import Section4 from './Components/Section4/Section4'
 import Section5 from './Components/Section5/Section5'
 import Section6 from './Components/Section6/Section6'
 import Section7 from './Components/Section7/Section7'
+import LocomotiveScroll from 'locomotive-scroll'
+
 const App = () => {
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector('[data-scroll-container]'),
+      smooth: true,
+    })
+
+    return () => {
+      scroll.destroy()
+    }
+  }, [])
+
   return (
-    <div className="App">
+    <div className="App" data-scroll-container>
       <Navbar />
       <Section1 />
       <Section2 />
